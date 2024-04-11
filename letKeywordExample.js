9ef59469f1978bf0b95649eb9c16b3ab13bfe47d
SiyyadriSrineeharika <2000032075@kluniversity.in>
2024-04-11 20:36:18 +0530

    upto 11th aprl 5 pm

---
+++ letKeywordExample.js
@@ -0,0 +1,9 @@
+// your code goes here
+let x=10;
+if(true){ // this is an if statement with a condition that is always true.
+    let y=20;
+    console.log(x); //output: 10
+    console.log(y); //output: 20
+}
+console.log(x); //output: 10
+console.log(y); //error: y is not defined
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+const myHeading=document.querySelector("h1");// here we are using querySelector to grab the reference of the h1 then storing it in a variable called myHeading.
+//similar to what we do using css selectors.
+//here we are selecting the element h1, when we want to something to a element we have to select it first.
+myHeading.textContent="Hello World!";//here we are setting(changing already existing content) the content using textContent property.
